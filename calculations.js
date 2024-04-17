af9653b0d71f206c3a434937332288c845fa1a4b
Viktoryia Astapuk <viktoryia.astapuk@gmail.com>
2024-04-17 20:22:29 +0300

    updated calculations.js task 2.2.1

--- calculations.js
+++ calculations.js
@@ -3,15 +3,15 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
+function decreaseBalance(x, y) {
 x = x - y;
 return x;
 }
 
+
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +21,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
