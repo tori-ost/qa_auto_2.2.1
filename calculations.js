975064461b20e22e09748298d49734c56e8f63da
Viktoryia Astapuk <viktoryia.astapuk@gmail.com>
2024-04-17 23:02:18 +0300

    Updated calculations.js task 2.2.4

--- calculations.js
+++ calculations.js
@@ -19,7 +19,31 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+//task 2.2.4
+
+function getPreIncrement(x) {
+  return ++x
+}
+
+function getPostIncrement(x) {
+  return x++
+}
+
+function getPreDecrement(x) {
+  return --x
+}
+function getPostDecrement(x) {
+  return x--
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(getPreIncrement(45));
+console.log(getPostIncrement(4655));
+console.log(getPreDecrement(17));
+console.log(getPostDecrement(190));
+
+
+
